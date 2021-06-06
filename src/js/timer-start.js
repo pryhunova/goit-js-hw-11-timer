45ead6ddbdd6de2250a6ee665042ce83feedf8a7
Nata Pryhunova <prygunova@gmail.com>
2021-06-07 01:59:19 +0300

    Таймер

---
+++ src/js/timer-start.js
@@ -0,0 +1,8 @@
+import { CountdownTimer } from './timer';
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: '6 Dec 2021',
+});
+
+timer.init();
---
+++ src/js/timer.js
@@ -0,0 +1,34 @@
+
+export class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.targetDate = new Date(targetDate);
+
+    this.days = document.querySelector(`${selector} .value[data-value="days"]`);
+    this.hours = document.querySelector(`${selector} .value[data-value="hours"]`);
+    this.minutes = document.querySelector(`${selector} .value[data-value="mins"]`);
+    this.seconds = document.querySelector(`${selector} .value[data-value="secs"]`);
+  }
+
+  init() {
+    setInterval(() => this._countDoun(), 1000)
+  }
+
+  _updateTime(totalSeconds) {
+    this.days.textContent = Math.floor(totalSeconds / 3600 / 24);
+    this.hours.textContent = this._formatTime(Math.floor((totalSeconds / 3600) % 24));
+    this.minutes.textContent = this._formatTime(Math.floor((totalSeconds / 60) % 60));
+    this.seconds.textContent = this._formatTime(Math.floor(totalSeconds % 60));
+  }
+
+  _countDoun() {
+    const currentDate = new Date();
+    const totalSeconds = (this.targetDate - currentDate) / 1000;
+    this._updateTime(totalSeconds)
+  }
+  _formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+}
+
+
+
